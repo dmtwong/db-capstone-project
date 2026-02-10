c526b7c3af65e940ed788c178d43bee5c9cc1752
David Wong <mingtak.wong1986@gmail.com>
2026-02-10 13:51:26 -0500

    feat(DB): reiterate Customer table schema (Cust) based on sample records

--- sql/v_1_cust_schema.sql
+++ sql/v_1_cust_schema.sql
@@ -3,13 +3,16 @@ USE littleLemonProject;
 DROP TABLE if exists Cust; 
 
 CREATE TABLE Cust(
-    cust_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
+    cust_id VARCHAR(11) PRIMARY KEY,
+    cust_name VARCHAR(50) NOT NULL,
     phone VARCHAR(20),
-    email VARCHAR(100) NULL, 
+    email VARCHAR(100) NULL
+);
+
+/*
     address VARCHAR(255) NULL,       
     visits_cnt INT DEFAULT 0,
     isBook TINYINT DEFAULT 0,
     tot_spent DECIMAL(8, 2) DEFAULT 0.00
-);
+*/
+
