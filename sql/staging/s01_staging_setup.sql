51c86e64bb99d4fa330d5d65d783d30dbeafdc00
David Wong <mingtak.wong1986@gmail.com>
2026-02-11 20:57:11 -0500

    feat(migration): restructure subfolders and revised the raw table schema to import raw from data/landing

--- sql/staging/s01_staging_setup.sql
+++ sql/staging/s01_staging_setup.sql
@@ -1,8 +1,10 @@
 USE littleLemonProject;
 DROP TABLE IF EXISTS Staging_Raw_Data;
 
+-- SHOW VARIABLES LIKE 'max_allowed_packet';
+-- utf8mb4 standard for modern databases because it supports all international characters, symbols, and emojis.
 CREATE TABLE Staging_Raw_Data (    
-    record_id INT, -- likely surrogate     
+    record_id INT PRIMARY KEY, -- likely surrogate     
     -- Order (v5_orders_schema.sql and v_6_orderdeliverystatus_schema)
     Order_ID VARCHAR(20),       -- OrderID
     Order_Date VARCHAR(50),     
@@ -28,4 +30,32 @@ CREATE TABLE Staging_Raw_Data (
     Desert_Name VARCHAR(100),   
     Drink VARCHAR(100),         
     Sides VARCHAR(100)          
-)
+) ENGINE=InnoDB DEFAULT CHARSET= utf8mb4; -- utf8mb4_unicode_ci; -- utf8mb4; 
+
+SET GLOBAL local_infile = 1;
+-- ALTER TABLE Staging_Raw_Data CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+LOAD DATA LOCAL INFILE 'C:\\Users\\mingt\\Documents\\db-capstone-project\\data\\landing\\database_project_v2.csv'
+INTO TABLE Staging_Raw_Data
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; -- Skips the header
+
+SELECT count(*)
+FROM staging_raw_data;
+
+SELECT *
+FROM Staging_Raw_Data 
+WHERE record_id IN (80, 81, 89, 90, 91, 92);
+
+SELECT * 
+FROM Staging_Raw_Data
+WHERE record_id IN (10, 11, 12, 31, 32, 33, 40, 41, 42, 43); 
+
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Staging_Raw_Data 
+SET Sales = REPLACE(REPLACE(Sales, '$', ''), ',', '')
+WHERE Sales LIKE '%$%' OR Sales LIKE '%,%';
+
+SET SQL_SAFE_UPDATES = 1;
+SET GLOBAL local_infile = 0;
